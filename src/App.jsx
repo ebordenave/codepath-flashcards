4e027086d5dea0ac38dc20f7ca24733aa097a9d0
ebordenave <edward.bordenave@gmail.com>
2024-03-04 00:04:21 -0500

    start wireframe

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,18 @@ function App() {
 
   return (
     <>
+      <div className="wrapper">
+        <section className="header">
+          <h2>
+            Ace your Tech Interview, today!
+          </h2>
+          <p>How well do you know Data Structures and Algorithms? Test your
+            might!</p>
+        </section>
+        <section className="flashcardContainer">flashcards here somewhere
+        </section>
+        <section className="arrowContainer">arrows here</section>
+      </div>
     </>
   )
 }
