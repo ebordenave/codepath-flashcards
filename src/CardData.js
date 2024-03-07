47354bf7de52bb989d05d054d57cc8919fe9a0f8
ebordenave <edward.bordenave@gmail.com>
2024-03-06 23:10:19 -0500

    template out the card object data

--- src/CardData.js
+++ src/CardData.js
@@ -1,9 +1,14 @@
 export const CardData = [
-  { front: "front: question here 1", back: "back: answer here 1", difficulty: "easy"},
-  { front: "front: question here 2", back: "back: answer here 2"},
-  { front: "front: question here 3", back: "back: answer here 3"},
-  { front: "front: question here 4", back: "back: answer here 4"},
-  { front: "front: question here 5", back: "back: answer here 5"},
-  { front: "front: question here 6", back: "back: answer here 6"},
+  { front: "front: question here 1", back: "back: answer here 1", difficulty: "easy", image: "placeholder"},
+  { front: "front: question here 2", back: "back: answer here 2", difficulty: "medium", image: "placeholder"},
+  { front: "front: question here 3", back: "back: answer here 3", difficulty: "hard", image: "placeholder"},
+  { front: "front: question here 4", back: "back: answer here 4", difficulty: "easy", image: "placeholder"},
+  { front: "front: question here 5", back: "back: answer here 5", difficulty: "medium", image: "placeholder"},
+  { front: "front: question here 6", back: "back: answer here 6", difficulty: "hard", image: "placeholder"},
+  { front: "front: question here 7", back: "back: answer here 7", difficulty: "easy", image: "placeholder"},
+  { front: "front: question here 8", back: "back: answer here 8", difficulty: "medium", image: "placeholder"},
+  { front: "front: question here 9", back: "back: answer here 9", difficulty: "hard", image: "placeholder"},
+  { front: "front: question here 10", back: "back: answer here 10", difficulty: "easy", image: "placeholder"},
+
 ]
 // export default CardData
