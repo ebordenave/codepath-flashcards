f979d0c2ad8a3c3e424be6e0b72f9740b30504ba
ebordenave <edward.bordenave@gmail.com>
2024-03-06 23:00:06 -0500

    add difficulty property to carddata

--- src/CardData.js
+++ src/CardData.js
@@ -1,5 +1,5 @@
 export const CardData = [
-  { front: "front: question here 1", back: "back: answer here 1"},
+  { front: "front: question here 1", back: "back: answer here 1", difficulty: "easy"},
   { front: "front: question here 2", back: "back: answer here 2"},
   { front: "front: question here 3", back: "back: answer here 3"},
   { front: "front: question here 4", back: "back: answer here 4"},
